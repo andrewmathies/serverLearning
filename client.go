c8dfbc5b682fbbfc7bc69f7244262588521c1e12
Andrew Mathies <amathies@clearblade.com>
2018-06-24 21:25:50 -0500

    added protocol id to client

--- client.go
+++ client.go
@@ -1,15 +1,33 @@
 package main
 
 import (
-        "fmt"
-        "net"
-        "time"
-	"strconv"
+    "fmt"
+    "net"
+    "time"
 	"bufio"
+	"hash/fnv"
+	"encoding/gob"
+	"bytes"
 )
-//
+
+// total 20 bytes
+type message struct {
+	ProtocolID uint32 // 32 bits = 4 bytes
+	Payload [16]byte // 16 bytes 
+}
+
+func hash(s string) uint32 {
+        h := fnv.New32a()
+        h.Write([]byte(s))
+        return h.Sum32()
+}
+
+// 20 bytes * 8 bits = 128
+var messageSize = 160
 
 func main() {
+	//INITIALIZATION
+
 	/*
 	localAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
 
@@ -18,48 +36,62 @@ func main() {
 		return
 	}
 	*/
-	serverAddr, err := net.ResolveUDPAddr("udp", "13.58.214.125:10001")
 
-	if err != nil {
-		fmt.Printf("some error %v\n", err)
+	serverAddr, addrErr := net.ResolveUDPAddr("udp", "13.58.214.125:10001")
+
+	if addrErr != nil {
+		fmt.Printf("some error %v\n", addrErr)
 		return
 	}
 
-        conn, err := net.DialUDP("udp", nil, serverAddr)
+    conn, dialErr := net.DialUDP("udp", nil, serverAddr)
 
-        if err != nil {
-                fmt.Printf("some error %v\n", err)
-                return
-        }
+    if dialErr != nil {
+            fmt.Printf("some error %v\n", dialErr)
+            return
+    }
 
 	defer conn.Close()
 	fmt.Printf("initialized\n")
 
-	i := 0
-	for {
-		msg := strconv.Itoa(i)
-		i++
-		buf := []byte(msg)
-		_, err := conn.Write(buf)
-		
-		if err != nil {
-			fmt.Printf(msg, err)
-			return
-		}
-		
-		readBuf := make([]byte, 1024)
-		placeholder, err := bufio.NewReader(conn).Read(readBuf)
+	// Building packet
 
-		if err != nil {
-			fmt.Printf("error reading! ", err)
-			fmt.Printf("plus this! ", placeholder)
-			return
-		} else {
-			fmt.Printf("%s\n\n", readBuf)
-		}
+	msg := new(message)
 
-		time.Sleep(time.Second * 1)
+	msg.ProtocolID = hash("Granada1.0")
+	fmt.Printf("Our ProtocolID is: %v\n", msg.ProtocolID)
+	var buffer [16]byte
+	copy(buffer[:], "hello server!")
+	msg.Payload = buffer
+
+	// Encoding packet
+	var convertedMsg bytes.Buffer
+	if err := gob.NewEncoder(&convertedMsg).Encode(msg); err != nil {
+		fmt.Printf("encode err: ", err)
+		return
 	}
+
+	// Writing packet
+	_, writeErr := conn.Write(convertedMsg.Bytes())
+	
+	if writeErr != nil {
+		fmt.Printf("write error ", writeErr)
+		return
+	}
+	
+	// Reading response
+	readBuf := make([]byte, messageSize)
+	placeholder, readErr := bufio.NewReader(conn).Read(readBuf)
+
+	if readErr != nil {
+		fmt.Printf("error reading! ", readErr)
+		fmt.Printf("plus this! ", placeholder)
+		return
+	} else {
+		fmt.Printf("%s\n\n", readBuf)
+	}
+
+	time.Sleep(time.Second * 1)
 }		
 
 /*
